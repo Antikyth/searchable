8a08760c8a92885fb3b5946863b6817454eb90b8
Antikyth <104020300+Antikyth@users.noreply.github.com>
2023-09-01 10:27:22 +1200

    add MPL-2.0 license headers and edit license field in quilt.mod.json

--- src/main/java/io/github/antikyth/searchable/access/ILanguageOptionsScreenMixin.java
+++ src/main/java/io/github/antikyth/searchable/access/ILanguageOptionsScreenMixin.java
@@ -1,7 +1,13 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package io.github.antikyth.searchable.access;
 
 import net.minecraft.client.gui.widget.TextFieldWidget;
 
 public interface ILanguageOptionsScreenMixin {
-    TextFieldWidget getSearchBox();
+	TextFieldWidget getSearchBox();
 }
--- src/main/java/io/github/antikyth/searchable/access/ILanguageSelectionListWidgetMixin.java
+++ src/main/java/io/github/antikyth/searchable/access/ILanguageSelectionListWidgetMixin.java
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package io.github.antikyth.searchable.access;
 
 import net.minecraft.client.gui.screen.option.LanguageOptionsScreen.LanguageSelectionListWidget.LanguageEntry;
@@ -6,7 +12,7 @@ import net.minecraft.client.resource.language.LanguageDefinition;
 import java.util.Map;
 
 public interface ILanguageSelectionListWidgetMixin {
-    void filter(String query, Map<String, LanguageDefinition> languages);
+	void filter(String query, Map<String, LanguageDefinition> languages);
 
-    LanguageEntry getSelectedLanguage();
+	LanguageEntry getSelectedLanguage();
 }

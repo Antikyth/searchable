2b0c9b1403d4f62debf5fef2ba24602a6470219f
Antikyth <104020300+Antikyth@users.noreply.github.com>
2023-08-31 13:28:29 +1200

    add search box to key binds screen

---
+++ src/main/java/io/github/antikyth/searchable/mixin/KeyBindListWidgetMixin.java
@@ -0,0 +1,21 @@
+package io.github.antikyth.searchable.mixin;
+
+import io.github.antikyth.searchable.Searchable;
+import net.minecraft.client.gui.widget.option.KeyBindListWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(KeyBindListWidget.class)
+public class KeyBindListWidgetMixin {
+	@ModifyArg(method = "<init>", at = @At(
+			value = "INVOKE",
+			target = "net/minecraft/client/gui/widget/ElementListWidget.<init> (Lnet/minecraft/client/MinecraftClient;IIIII)V"
+	), index = 3)
+	private static int adjustTopCoord(int top) {
+		Searchable.LOGGER.debug("moving keybinds list down by 28px...");
+
+		// 12 pixels lower to match the normal header height (32px), then 16 lower to make space for the search box.
+		return top + 12 + 16;
+	}
+}
---
+++ src/main/java/io/github/antikyth/searchable/mixin/KeyBindScreenMixin.java
@@ -0,0 +1,42 @@
+package io.github.antikyth.searchable.mixin;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.option.GameOptionsScreen;
+import net.minecraft.client.gui.screen.option.KeyBindsScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(KeyBindsScreen.class)
+public class KeyBindScreenMixin extends GameOptionsScreen {
+	@Unique
+	public TextFieldWidget searchBox;
+
+	// Mixin will ignore this - required because of extending `GameOptionsScreen`
+	public KeyBindScreenMixin(Screen parent, GameOptions gameOptions, Text title) {
+		super(parent, gameOptions, title);
+	}
+
+	@Inject(method = "init", at = @At("HEAD"))
+	public void onInit(CallbackInfo ci) {
+		this.searchBox = new TextFieldWidget(this.textRenderer, this.width / 2 - 100, 22, 208, 20, this.searchBox, Text.translatable("controls.keybinds.search"));
+
+		this.addSelectableChild(this.searchBox);
+		this.setInitialFocus(this.searchBox);
+	}
+
+	@Inject(method = "render", at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/gui/widget/option/KeyBindListWidget;render(Lnet/minecraft/client/gui/GuiGraphics;IIF)V",
+			shift = At.Shift.AFTER
+	))
+	public void onRender(GuiGraphics graphics, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+		this.searchBox.drawWidget(graphics, mouseX, mouseY, delta);
+	}
+}
